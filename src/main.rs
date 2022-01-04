b3013d4eda66265008e471e13b5623b0c0cc700a
sinotca529-wsl <sinotca35963@gmail.com>
2022-01-04 23:59:35 +0900

    impl : change the type of return value of main from () to Result.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use app::App;
-use command::keyinput::KeyInput;
+use app::{App, AppError};
+use command::{keyinput::KeyInput, CommandStream};
+use image::ImageError;
+use thiserror::Error;
 
 use crate::image::Image;
 
@@ -8,15 +10,30 @@ mod command;
 mod image;
 mod widget;
 
-fn main() {
+#[derive(Error, Debug)]
+pub enum MainError<E: 'static + std::error::Error + std::fmt::Debug> {
+    #[error("Error occurred in the app.")]
+    AppError(#[source] AppError<E>),
+
+    #[error("Error occurred in Image.")]
+    ImageError(#[source] ImageError),
+
+    #[error("Incorrect argument: `{0}`")]
+    ArgError(String),
+}
+
+fn main() -> Result<(), MainError<<KeyInput as CommandStream>::Error>> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
-        println!("Please send a png file's path");
-        return;
+        return Err(MainError::ArgError("Please send a png file's path".into()));
     }
 
     let img_path = &args[1];
 
-    let img = Image::open(img_path).unwrap();
-    App::new(img, KeyInput::new()).run().unwrap();
+    let img = Image::open(img_path).map_err(MainError::ImageError)?;
+    App::new(img, KeyInput::new())
+        .run()
+        .map_err(MainError::AppError)?;
+
+    Ok(())
 }
